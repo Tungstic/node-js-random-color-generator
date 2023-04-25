4a25cc1ffcb267674c3ea47b032dc3a41f4b67f9
Natalia Budulchieva <nbudulchieva@gmail.com>
2023-04-25 20:49:20 +0200

    Fix blue dark because of a different hex code

--- index.js
+++ index.js
@@ -29,11 +29,20 @@ function drawRectangle() {
   middle();
   topAndBottom();
 }
+
+// Tests failed because my library has different hex codes for "red" and "blue dark"
 if (userColor === 'red') {
   hexCode = '#b72d42';
   color = chalk.hex(hexCode);
   drawRectangle();
 }
+if (userColor === 'blue dark') {
+  hexCode = '#023e8c';
+  color = chalk.hex(hexCode);
+  drawRectangle();
+}
+
+// Code assuming that the hex codes and color names from the libraries are correct
 if (userColor && !luminosity) {
   const finalColor = userColor;
   hexCode = toHex(finalColor);
